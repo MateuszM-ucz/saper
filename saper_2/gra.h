b14dfe3ab5ce307261cb19117e40f879cab0f91e
MateuszM-ucz <01197834@pw.edu.pl>
2025-01-27 11:40:24 +0100

    Delete saper_2 directory

--- saper_2/gra.h
+++
@@ -1,8 +0,0 @@
-#ifndef GRA_H_IS_INCLUDED_
-#define GRA_H_IS_INCLUDED_
-
-#include "plansza.h"
-
-void gra();
-
-#endif
--- saper_2/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "gra.h"
-
-int main() {
-    gra();
-    return 0;
-}
